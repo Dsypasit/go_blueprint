77f29d9cc514c33832d09981b070e4f6e311d151
dsypasit <ong.pasit@gmail.com>
2023-01-03 18:21:01 +0700

    feat: add counter function

---
+++ socialpoll/counter/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/nsqio/go-nsq"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var fatalErr error
+
+func fatal(e error) {
+	fmt.Println(e)
+	flag.PrintDefaults()
+	fatalErr = e
+}
+
+var counts map[string]int
+var countsLock sync.Mutex
+
+const updateDuration = 1 * time.Second
+
+func main() {
+	defer func() {
+		if fatalErr != nil {
+			os.Exit(1)
+		}
+	}()
+
+	log.Println("Connecting to database...")
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		fatal(err)
+		return
+	}
+	defer func() {
+		log.Println("Closing database connection...")
+		db.Close()
+	}()
+	pollData := db.DB("ballots").C("polls")
+
+	log.Println("Connecting to nsq...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(message.Body)
+		counts[vote]++
+		return nil
+	}))
+
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
+}
+
+func doCount(countsLock *sync.Mutex, counts *map[string]int, pollData *mgo.Collection) {
+	countsLock.Lock()
+	defer countsLock.Unlock()
+	if len(*counts) == 0 {
+		log.Println("No new votes, skipping database update")
+	}
+	log.Println("Updating database...")
+	log.Println(*counts)
+	ok := true
+	for option, count := range *counts {
+		sel := bson.M{"options": bson.M{"$in": []string{option}}}
+		up := bson.M{"$inc": bson.M{"results." + option: count}}
+		if _, err := pollData.UpdateAll(sel, up); err != nil {
+			log.Println("failed to update:", err)
+			ok = false
+		}
+	}
+	if ok {
+		log.Println("Finished updating database...")
+		*counts = nil
+	}
+}
