26a5f596316744f832204a246d4008addbee94cc
dsypasit <ong.pasit@gmail.com>
2023-01-06 22:27:08 +0700

    feat: implement handlePollsGet

--- api/polls.go
+++ api/polls.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -15,7 +16,7 @@ type poll struct {
 	APIKey  string         `json:"api_key"`
 }
 
-func (s *Server) handlePolls(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HandlePolls(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		s.handlePollsGet(w, r)
@@ -31,7 +32,21 @@ func (s *Server) handlePolls(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) handlePollsGet(w http.ResponseWriter, r *http.Request) {
-	respondErr(w, r, http.StatusInternalServerError, errors.New("not implmented"))
+	session := s.db.Copy()
+	defer session.Close()
+	c := session.DB("ballots").C("polls")
+	var q *mgo.Query
+	p := NewPath(r.URL.Path)
+	if p.HasID() {
+		q = c.FindId(bson.ObjectIdHex(p.ID))
+	} else {
+		q = c.Find(nil)
+	}
+	var result []*poll
+	if err := q.All(&result); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, err)
+	}
+	respond(w, r, http.StatusOK, &result)
 }
 
 func (s *Server) handlePollsPost(w http.ResponseWriter, r *http.Request) {
